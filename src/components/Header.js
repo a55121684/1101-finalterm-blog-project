8d506d433e19b1a5cf7de0ccc162a30d1e885003
Shin <209410157@gms.tku.edu.tw>
2022-01-04 17:18:53 +0800

    feat(homepage): finish header layout

---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from "react";
+import logo from "../assets/blog_logo.png";
+import "../styles/Header.scss";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="header_left">
+        <img className="header_logo" src={logo} alt="" />
+        <h2 className="header_title">Personal Blog</h2>
+      </div>
+      <div className="header_right">
+        <Link className="header_navItem" to="/">
+          Home
+        </Link>
+        <Link className="header_navItem" to="/">
+          About
+        </Link>
+        <Link className="header_navItem" to="/">
+          New Post
+        </Link>
+        <Link className="header_navItem" to="/">
+          Login
+        </Link>
+      </div>
+    </div>
+  );
+}
