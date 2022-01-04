e78bd2b4f89f85e609d8eefe4655a33980ecc821
Shin <bear55121677@gmail.com>
2022-01-04 19:26:35 +0800

    feat(homepage): make header & personInfo sticky

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,85 @@
 import React from "react";
 import Header from "../components/Header";
+import PersonInfo from "../components/PersonInfo";
+import "../styles/HomePage.scss";
 
 export default function HomePage() {
   return (
     <div>
-      <Header />
+      <div className="homepage_header">
+        <Header />
+      </div>
+      <div className="homepage_content">
+        <div className="homepage_person">
+          <PersonInfo />
+        </div>
+        <div className="homepage_post">
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+        </div>
+      </div>
     </div>
   );
 }
