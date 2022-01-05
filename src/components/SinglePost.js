f4ad1bc9efc955a0ba85c8c8741c56f14e6de4ee
Shin <bear55121677@gmail.com>
2022-01-05 20:26:41 +0800

    feat(homepage): finish singlePost layout & some change about homepage

---
+++ src/components/SinglePost.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/SinglePost.scss";
+
+export default function SinglePost({ title, content, img, date }) {
+  return (
+    <div className="post">
+      <h5 className="post_title">{title}</h5>
+      <div className="post_imgBox">
+        <img className="post_img" src={img} alt="" />
+      </div>
+      <div className="post_contentBox">
+        <div className="post_content">{content}</div>
+      </div>
+      <div className="post_dateBox">
+        <div className="post_date">{date}</div>
+      </div>
+    </div>
+  );
+}
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,45 @@
 import React from "react";
 import Header from "../components/Header";
 import PersonInfo from "../components/PersonInfo";
+import SinglePost from "../components/SinglePost";
 import "../styles/HomePage.scss";
 
 export default function HomePage() {
+  const testData = [
+    {
+      id: 1,
+      title: "Test Post",
+      content:
+        "2008年12月26日，瑞雪兆豐年，加拿大溫哥華菩提禪堂的二級班上同樣是一派喜慶。剛剛風塵僕僕遠道歸來的金菩提宗師未及休息，就和來自世界各地的學員們歡聚一堂，談法論道。宗師以自己滑雪的親身經歷，幽默地告訴大家，要以快樂的心跨越自己心裡的障礙，向更高、更新的境界邁進。本片幽默風趣，和家人朋友一同觀看，可以在笑聲中感受到一份超越困難的信心。",
+      img: "https://media-cdn.tripadvisor.com/media/photo-s/10/c4/23/16/highland-view-bed-and.jpg",
+      date: "2022/01/05",
+    },
+    {
+      id: 1,
+      title: "Test Post",
+      content:
+        "2008年12月26日，瑞雪兆豐年，加拿大溫哥華菩提禪堂的二級班上同樣是一派喜慶。剛剛風塵僕僕遠道歸來的金菩提宗師未及休息，就和來自世界各地的學員們歡聚一堂，談法論道。宗師以自己滑雪的親身經歷，幽默地告訴大家，要以快樂的心跨越自己心裡的障礙，向更高、更新的境界邁進。本片幽默風趣，和家人朋友一同觀看，可以在笑聲中感受到一份超越困難的信心。",
+      img: "https://media-cdn.tripadvisor.com/media/photo-s/10/c4/23/16/highland-view-bed-and.jpg",
+      date: "2022/01/05",
+    },
+    {
+      id: 1,
+      title: "Test Post",
+      content:
+        "2008年12月26日，瑞雪兆豐年，加拿大溫哥華菩提禪堂的二級班上同樣是一派喜慶。剛剛風塵僕僕遠道歸來的金菩提宗師未及休息，就和來自世界各地的學員們歡聚一堂，談法論道。宗師以自己滑雪的親身經歷，幽默地告訴大家，要以快樂的心跨越自己心裡的障礙，向更高、更新的境界邁進。本片幽默風趣，和家人朋友一同觀看，可以在笑聲中感受到一份超越困難的信心。",
+      img: "https://media-cdn.tripadvisor.com/media/photo-s/10/c4/23/16/highland-view-bed-and.jpg",
+      date: "2022/01/05",
+    },
+    {
+      id: 1,
+      title: "Test Post",
+      content:
+        "2008年12月26日，瑞雪兆豐年，加拿大溫哥華菩提禪堂的二級班上同樣是一派喜慶。剛剛風塵僕僕遠道歸來的金菩提宗師未及休息，就和來自世界各地的學員們歡聚一堂，談法論道。宗師以自己滑雪的親身經歷，幽默地告訴大家，要以快樂的心跨越自己心裡的障礙，向更高、更新的境界邁進。本片幽默風趣，和家人朋友一同觀看，可以在笑聲中感受到一份超越困難的信心。",
+      img: "https://media-cdn.tripadvisor.com/media/photo-s/10/c4/23/16/highland-view-bed-and.jpg",
+      date: "2022/01/05",
+    },
+  ];
+
   return (
     <div>
       <div className="homepage_header">
@@ -14,70 +50,18 @@ export default function HomePage() {
           <PersonInfo />
         </div>
         <div className="homepage_post">
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
-          <div>1</div>
+          {testData.map((post) => {
+            return (
+              <SinglePost
+                className="post"
+                key={post.id}
+                title={post.title}
+                content={post.content}
+                img={post.img}
+                date={post.date}
+              />
+            );
+          })}
         </div>
       </div>
     </div>
